3431510f02457635a0b8bfb322e40d1244731064
AnandaMontoly <amontoly@smith.edu>
2020-12-17 03:35:45 -0500

    when did it connect to git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Car extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {color: "red",
+                  music:"jamming"};
+  }
+  mood = () => {
+    ///of note -  this will only change the listed values right here despite looking like it replaces the json object
+    if (this.state.music == "jamming"){
+      this.setState({music:"sad"});
+    }else {
+      this.setState({music:"jamming"});
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <h2>My name is Car. I am {this.state.color}. I am a {this.props.cartype.brand} car.</h2>
+        <p>I low key be {this.state.music}</p>
+        <button
+          type="button"
+          onClick={this.mood}
+          >change your mood</button>
+      </div>
+    )
+
+  }
 }
-
-export default App;
+class Garage extends React.Component {
+  constructor () {
+    super();
+  }
+  render () {
+    const cartype = {brand:"Toyota", color:"Blue"}
+    return (
+      <div>
+        <h2> Guess what its ya boy garage </h2>
+        <Car cartype={cartype}/>
+      </div>
+    )
+  }
+}
+export default Car;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,126 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Car from "./app.js";
+import './Site.scss';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Tab from 'react-bootstrap/Tab';
+import Nav from 'react-bootstrap/Nav';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class HomeText extends React.Component {
+  constructor () {
+    super();
+  }
+  render () {
+    return (
+      <div>
+        <p>
+          Welcome to Ananda's Cool Home Page for her Cool Class
+        </p>
+      </div>
+    )
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class SuppMaterial extends React.Component {
+  constructor () {
+    super();
+  }
+  render () {
+    return (
+      <div>
+        <p>
+          Supplemental Material
+        </p>
+      </div>
+    )
+  }
+}
+
+class ClassText extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+  render () {
+    return (
+      <p style={{color:"red"}}>
+        God isn't finished with me yet.
+      </p>
+    )
+  }
+}
+class Tabbies extends React.Component {
+  constructor () {
+    super();
+  }
+  render() {
+    return (
+    <div id="encloses">
+      <Tab.Container id="pagecontent" defaultActiveKey="first">
+        <Row style={{marginLeft:"1px"}}>
+          <Col sm={3}>
+            <Nav variant="pills" className="flex-column">
+              <Nav.Item>
+                <Nav.Link eventKey="first">Home</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="second">First Class</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="supp">Resources</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+          <Col sm={9}>
+            <Tab.Content>
+              <Tab.Pane eventKey="first">
+                <HomeText />
+              </Tab.Pane>
+              <Tab.Pane eventKey="second">
+                <ClassText />
+              </Tab.Pane>
+              <Tab.Pane eventKey="supp">
+                <SuppMaterial />
+              </Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
+    </div>
+  )
+  }
+}
+class Header extends React.Component {
+  constructor() {
+    super();
+  }
+  render () {
+    return (
+      <div>
+        <Jumbotron fluid style={{backgroundColor:"#9fb0cc"}}>
+          <Container>
+            <h1>Intro to Intro to Data Structures</h1>
+            <h3>J-Term 2021</h3>
+          </Container>
+        </Jumbotron>
+      </div>
+    )
+  }
+}
+
+class Page extends React.Component {
+  constructor () {
+    super();
+  }
+  render () {
+    return (<div style={{backgroundColor:"#fffffc"}}>
+        <Header />
+        <Tabbies />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Page />, document.getElementById('root'));
